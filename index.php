2c8fa90b8fefe976bd1aa983843fbdf80004724b
vbnm7180 <vbnm7180@mail.ru>
2020-08-15 12:15:17 +0400

    master

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Старт сессии. Стартовать сессию надо в каждом файле в начале, где используются переменные $_SESSION
+session_start();
+
+//Если пуст массив с количеством элементов в корзине, создать пустой массив корзины в текущей сессии
+if (!isset($_SESSION['in_cart'])) {
+	$_SESSION['in_cart'] = array();
+}
+if (!isset($_SESSION['login']) && !isset($_SESSION['password'])) {
+$_SESSION['login']="";
+$_SESSION['password']="";
+}
+
+header("Location: http://retrogame/controllers/pageController.php?page_id=main");
